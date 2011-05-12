4c833cffd0babb3eb533f61ef5bd60c29dab12bd
ThomasHandorf <ThomasHandorf@localhost>
2011-05-12 10:56:16 +0200

    we will use a different data structure, the old files have been deleted

--- flowlayout/src/tdefs.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-/*typedef signed char int8; //!<ensured to be 8bit or larger
-typedef short int16; //!<ensured to be 16bit or larger
-typedef long int32; //!<ensured to be 32bit or larger*/
-typedef int8_t int8; //!<8 bit signed integer, for masks
-typedef int16_t int16; //!<16 bit signed integer
-typedef int32_t int32; //!<32 bit signed integer, for indexes
