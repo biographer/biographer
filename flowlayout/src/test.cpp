2e883cd5294d6468bf6b1f7ed0ec01bf833b7886
Acer King Wei <Acer.King.Wei@gmail.com>
2011-05-17 14:03:45 +0000

    modified

--- flowlayout/src/test.cpp
+++ flowlayout/src/test.cpp
@@ -2,27 +2,66 @@
 #include "network.h"
 
 int main(){
-  int i,j,k,x,y;
+  int i,j,k,x,y,R,Q;
+  int index;
+  char s[20];
+  char names[][20]={"substrate", "product", "catalyst", "activator", "inhibitor"};
   Network nw=Network();
-  vector<int>subs;subs.clear();
-  vector<int>prod;prod.clear();
-  vector<int>inhi;inhi.clear();
-  vector<int>cat;cat.clear();
-  vector<int>act;act.clear();
-  subs.push_back(2);subs.push_back(3);
-  prod.push_back(8);prod.push_back(9); 
-  cat.push_back(3);
-  inhi.push_back(6);
-  act.push_back(4);
-  nw.addReaction(0,&subs,&prod,&cat,&act,&inhi);
-  VI* a = nw.getNeighbors(0,catalyst);
-  cout<<nw.nodes->size()<<endl;
-  cout<<nw.edges->size()<<endl;
-  for(i=0;i<a->size();i++)cout<<(*a)[i]<<' ';
-  cout<<endl;
-  free(a);
-  system("pause");
-  
+  vector<int>subs;
+  vector<int>prod;
+  vector<int>cat;
+  vector<int>act;
+  vector<int>inhi;
+  freopen("data.txt","r",stdin);
+  freopen("summary.txt","w",stdout);
+  scanf("%d%d",&R,&Q);
+  while(R--){
+     gets(s);
+     gets(s);
+     gets(s);
+     scanf("%d",&index);
+     scanf("%d:",&k);
+     subs.clear();
+     while(k--){
+       scanf("%d",&x);
+       subs.push_back(x);
+     }
+     scanf("%d:",&k);
+     prod.clear();
+     while(k--){
+       scanf("%d",&x);
+       prod.push_back(x);
+     }
+     scanf("%d:",&k);
+     cat.clear();
+     while(k--){
+       scanf("%d",&x);
+       cat.push_back(x);
+     }
+     scanf("%d:",&k);
+     act.clear();
+     while(k--){
+       scanf("%d",&x);
+       act.push_back(x);
+     }
+     scanf("%d:",&k);
+     inhi.clear();
+     while(k--){
+       scanf("%d",&x);
+       inhi.push_back(x);
+     }      
+     nw.addReaction(index,&subs,&prod,&cat,&act,&inhi);
+  }
+  VI *a;
+  while(Q--){
+     scanf("%d%d",&x,&y);
+     printf("Reaction #%d, %s\n",x,names[y]);
+     a=nw.getNeighbors(x,(Edgetype)(y+2));
+     for(i=0;i<a->size();i++)printf("%d ",(*a)[i]);
+     printf("\n\n");
+     a->clear();
+     free(a);
+  }                          
   return 0;
 }
   
