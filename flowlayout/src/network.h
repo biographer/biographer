2eeccf53dcaba14a16f722817a650ee349e74126
Acer King Wei <Acer.King.Wei@gmail.com>
2011-05-17 14:03:09 +0000

    modified

--- flowlayout/src/network.h
+++ flowlayout/src/network.h
@@ -13,7 +13,7 @@ public:
    VE * edges;
      
    void addEdge(int from, int to, Edgetype type); //add in an edge "from"-->"to" of type "type".
-   void upto(int _size, Nodetype _type); //add in nodes, such that the number of nodes in the network reaches _size;      
+   void addNode(int index,Nodetype type); //add in a Node with specified index and type.     
    void addReaction(int index, const VI* substrates, const VI* products, const VI* catalysts, const VI* activators, const VI* inhibitors);   
    VI* getNeighbors(int nodeIndex, Edgetype type); //get the neigbors of a node.
    
