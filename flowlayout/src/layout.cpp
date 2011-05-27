5dc3264c8e40b4ea55fb8110acf351d555be4647
Acer King Wei <Acer.King.Wei@gmail.com>
2011-05-27 09:14:27 +0000

    layout algorithm, modified. Does not seem to work... Still under construction.

--- flowlayout/src/layout.cpp
+++ flowlayout/src/layout.cpp
@@ -1,84 +1,141 @@
 #include "network.h"
+#include "functions.h"
 #define inf 1e50
 #define zero 1e-12
 #define err 1e-5
 
-double Network::get_dij(int i, int j){ 
-   double x=(*nodes)[i].pts.width * (*nodes)[i].pts.width + (*nodes)[i].pts.height * (*nodes)[i].pts.height;
-   double y=(*nodes)[j].pts.width * (*nodes)[j].pts.width + (*nodes)[j].pts.height * (*nodes)[j].pts.height;
-   return (x+y)*4;
+float Network::get_dij1(int i, int j){ //ideal distance between adjacent nodes;
+   float x=(*nodes)[i].pts.width * (*nodes)[i].pts.width + (*nodes)[i].pts.height * (*nodes)[i].pts.height;
+   float y=(*nodes)[j].pts.width * (*nodes)[j].pts.width + (*nodes)[j].pts.height * (*nodes)[j].pts.height;
+   return (x+y)*2;
 }
 
-double Network::get_dis(int i, int j){
-   return sqrt((a[i].x-a[j].x)*(a[i].x-a[j].x)+(a[i].y-a[j].y)*(a[i].y-a[j].y));
+float Network::get_dij2(int i, int j){ //minimum distance between non-adjacent nodes;
+   float x=(*nodes)[i].pts.width * (*nodes)[i].pts.width + (*nodes)[i].pts.height * (*nodes)[i].pts.height;
+   float y=(*nodes)[j].pts.width * (*nodes)[j].pts.width + (*nodes)[j].pts.height * (*nodes)[j].pts.height;
+   return 0.2*(x+y);
 }
 
-double Network::calc_force(){
-   int n=a.size();
-   double sum=0.0;
-   int i,j;
-   double w,idis,dis;
-   for(i=0;i<n;i++)
-      for(j=0;j<n;j++)
-         if(i!=j){
-            idis=get_dij(i,j);
-            dis=get_dis(i,j);
-            if(fabs(idis)<zero)idis=zero;            
-            w = 1.0/idis/idis * (dis-idis)*(dis-idis);
-            sum+=w;
+float Network::calc_force_adj(){
+   float force=0.0;
+   float d,i_d,alpha,i_alpha,beta;
+   Point vec; //vector from pos[n1] to pos[n2];
+   int n1,n2,m,n,i;
+   Edgetype _type;
+   m=edges->size();
+   n=nodes->size();
+
+   for(i=0;i<m;i++){
+                    
+      n1=(*edges)[i].from; //reaction
+      n2=(*edges)[i].to; //compound;
+      _type=(*edges)[i].pts.type;
+      
+      vec=pos[n2]-pos[n1];
+      i_d=get_dij1(n1,n2);
+      d=dist(pos[n1],pos[n2]);
+      force+=((d-i_d)*(d-i_d)); //distantal force;
+   //   printf("%10.3f %10.3f\n",i_d,d);
+      
+      //distantal movements;
+      if(norm(vec)<zero){
+         if(_type==substrate)mov[n2].y+=(i_d/n); //substrates at top;
+         else if(_type==product)mov[n2].y-=(i_d/n);  //products at bottom;
+         else{
+            //other compounds on either side;
+            if(rand()%2)mov[n2].x+=(i_d/n);
+            else mov[n2].x-=(i_d)/n; 
          }
-   return sum;
-}       
-   
-double Network::get_new_pos(){
-   int n=a.size();
-   int i,j,k;
-   double deno=0.0, dis,idis,w;
-   b[0]=a[0];
-   for(i=1;i<n;i++){
-      b[i].x=b[i].y=0.0;
-      for(j=0;j<i;j++)
-         if(i!=j){
-            idis=get_dij(i,j);
-            dis=get_dis(i,j);
-            if(fabs(idis)<zero)idis=zero;
-            if(fabs(dis)<zero)dis=zero;
-            w=1.0/idis/idis * (b[j].x+idis*(a[i].x-a[j].x)/dis);
-            b[i].x+=w;
-            w=1.0/idis/idis * (b[j].y+idis*(a[i].y-a[j].y)/dis);
-            b[i].y+=w;
+      }
+      else{
+         mov[n2].x+=(vec.x*(i_d-d)/n);
+         mov[n2].y+=(vec.y*(i_d-d)/n);
+      }
+      
+      //angular force;
+      alpha=angle(vec);
+      if(_type==substrate){
+         i_alpha=0.5*PI;
+         beta=lim(i_alpha-alpha);
+      }         
+      else if(_type==product){
+         i_alpha=1.5*PI;
+         beta=lim(i_alpha-alpha);
+      }
+      else{ //other compounds, rotating to the nearer side.
+         i_alpha=0.0; beta=lim(0.0-alpha);
+         float i_alpha1=PI, beta1=lim(PI-alpha);
+         if(fabs(beta)>fabs(beta1)){
+            beta=beta1;
+            i_alpha=i_alpha1;
          }
+      }
+      force+=(i_d*i_d*sin(beta/2)); //angular force;
+      mov[n2]=mov[n2]+(to_left(vec,beta/n)-vec); //angular movement; 
    }
-}          
-    
-double Network::layout(){
+   
+   return force;
+}
+
+float Network::calc_force_nadj(){
+   int n1,n2,n=nodes->size();
+   float d,i_d,force=0.0;
+   Point vec;
+   
+   for(n1=0;n1<n;n1++)
+      for(n2=0;n2<n;n2++){
+                          
+         i_d=get_dij2(n1,n2);
+         d=dist(pos[n1],pos[n2]);
+         if(d>=i_d)continue; //include force and move nodes only if they are too close to each other;
+         
+         force+=((i_d-d)*(i_d-d)); //distantal force;
+         
+         vec=pos[n1]-pos[n2];
+         mov[n1].x+=(vec.x/n/n);mov[n1].y+=(vec.y/n/n);
+         mov[n2].x-=(vec.x/n/n);mov[n2].y-=(vec.y/n/n);
+      }
+      
+   return force;
+}
+         
+void Network::move_nodes(){
+   int n=nodes->size();
+   for(int i=0;i<n;i++){
+      pos[i]=pos[i]+mov[i];
+      mov[i].x=mov[i].y=0.0;
+   }
+}
+
+float Network::layout(){
        
-   //copying coordinates from nodes[] to a[];   
+   //copying coordinates from nodes[] to pos[];   
    int n=nodes->size(),i;   
-   a.resize(n);
-   b.resize(n);
+   pos.resize(n);
+   mov.resize(n);
    for(i=0;i<n;i++){
-      a[i].x=(*nodes)[i].pts.x;
-      a[i].y=(*nodes)[i].pts.y;
+      pos[i].x=(*nodes)[i].pts.x;
+      pos[i].y=(*nodes)[i].pts.y;
+      mov[i].x=mov[i].y=0.0;
    }   
    
-   double cur_force,pre_force=inf;  
+   float cur_force,pre_force=inf;  
    int k=0;
-    
-   while(true){
-     cur_force=calc_force();
-     if(pre_force-cur_force < pre_force*err)break;
-     get_new_pos();
-     a=b;
-     pre_force=cur_force;
-     k++;
-     if(k>1000)break;
+   
+   srand(time(0));
+   while(true){     
+     cur_force=calc_force_adj();
+     cur_force+=calc_force_nadj();
+     move_nodes();
+     printf("%0.3f\n",cur_force);
+     if((pre_force-cur_force)<pre_force*err)break;
+     pre_force=cur_force;    
    }
    
    //copying coordinations from a[] to nodes[];
    for(i=0;i<n;i++){
-      (*nodes)[i].pts.x=a[i].x;
-      (*nodes)[i].pts.y=a[i].y;
+      (*nodes)[i].pts.x=pos[i].x;
+      (*nodes)[i].pts.y=pos[i].y;
    }
    
    return cur_force;
