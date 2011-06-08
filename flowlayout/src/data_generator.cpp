8ef82f6a87b76efad7113030da4c82746d8d476c
acerwei <acer.king.wei@gmail.com>
2011-06-08 21:18:11 +0800

    fixed a silly mistake in the data generator.

--- flowlayout/src/data_generator.cpp
+++ flowlayout/src/data_generator.cpp
@@ -1,56 +1,48 @@
 #include "headers.h"
 #include "network.h"
-
+   int rea[1000],comp[1000];
+   int nr,nc;
 int main(){
-    FILE *fout=fopen("data.txt","w");
-    int R,Q,n; //number of reactions, queries, total number of nodes;
-    bool isreaction [1000]; //whether a node is a reaction.
-    memset(isreaction,false,sizeof(isreaction));
-    while(true){
-       printf("Input three integers: R, Q n. Number of reactions, queries, and total number of nodes. Make sure R<n/2\n");
-       scanf("%d%d%d",&R,&Q,&n);
-       if(R<n/2)break;
-       else printf("R should be less than n/2");
-    }
-    
-    fprintf(fout,"%d %d\n\n",R,Q);
-    
-    int x,y,k,index,m=20;
-    if (m>n/2)m=n/2;
-    srand(time(0));
-    for(int tr=1;tr<=R;tr++){
-       fprintf(fout,"Reaction #%d:\n",tr);
-       while(true){
-          index=rand()%n;
-          if(!isreaction[index]){
-            isreaction[index]=true;
-            break;
-          }
-       }
-       fprintf(fout,"%d\n",index);
-       for(int i=0;i<5;i++){
-          k=rand()%m;
-          fprintf(fout,"%d: ",k);
-          while(k--){
-             while(true){
-                x=rand()%n;
-                if(!isreaction[x])break;
-              }
-            fprintf(fout,"%d ",x);
-          }
-          fprintf(fout,"\n");
-       }
-       fprintf(fout,"\n");
-    }
-   fprintf(fout,"\n\n");
-   for(int tq=1;tq<=Q;tq++){
-      //fprintf(fout, "Query #%d:  ",tq);
-      while(true){
-         x=rand()%n;
-         if(isreaction[x])break;
-      }
-      y=rand()%5;
-      fprintf(fout, "%d %d\n",x,y);
+   printf("input three integers cn n m : number of compartments, nodes and edges\n");
+   freopen("data.txt","w",stdout);
+   int cn,n,m;
+   int i,j,k,x,y,lnk;
+   char t_edge[][10]={"Substrate", "Product", "Catalyst", "Activator", "Inhibitor"};
+   srand(time(0));
+   scanf("%d",&cn);
+   printf("%d\n",cn);
+   for(i=0;i<cn;i++)
+      printf("%d Compartment-%d\n",i,i);
+   printf("///\n");
+   scanf("%d",&n);
+   printf("%d\n",n);
+   nr=nc=0;
+   for(i=0;i<n;i++){
+      printf("%d\n",i);
+      lnk=rand()%10;
+      if(lnk==0)lnk=0;
+      else if(lnk<7)lnk=1;
+      else lnk=2;
+      if(lnk==0)printf("Other\nNodes-%d\n",i);
+      else if(lnk==1)printf("Compound\nCompound-%d\n",i);
+      else printf("Reaction\nReaction-%d\n",i);
+      if(lnk==1)comp[nc++]=i;
+      else if(lnk==2)rea[nr++]=i;
+      printf("%d\n",rand()%cn);
+      printf("%0.2f\n",0.0+rand()%3);
+      printf("%0.2f\n",0.0+rand()%3);
+      printf("%0.2f\n",1.0+rand()%5);
+      printf("%0.2f\n",1.0+rand()%5);
+      printf("0.00\n");
+   }
+   scanf("%d",&m);
+   printf("///\n");
+   printf("%d\n",m);
+   while(m--){
+      lnk=rand()%5;
+      x=rand()%nr;
+      y=rand()%nc;
+      printf("%s %d %d\n",t_edge[lnk],rea[x],comp[y]);
    }
    return 0;
 }
