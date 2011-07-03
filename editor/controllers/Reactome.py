9207a60209677dc1ce2b7f35dacfb97674a5e535
matthias bock <matthias.bock@hu-berlin.de>
2011-07-03 12:44:34 +0200

    SBML importer improved

--- editor/controllers/Reactome.py
+++ editor/controllers/Reactome.py
@@ -12,8 +12,10 @@ def importer():
 def upload():
 	session.RSI = request.vars.RSI
 	connection = httplib.HTTPConnection("www.reactome.org")
-	connection.request("GET", "/cgi-bin/eventbrowser_st_id?ST_ID="+str(session.RSI))
+	connection.request("GET", "/cgi-bin/eventbrowser?DB=gk_current&ID=168254&ZOOM=2") #"/cgi-bin/eventbrowser_st_id?ST_ID="+str(session.RSI))
 	page = connection.getresponse().read()
+	open("/tmp/reactome.html","w").write(page)
+	print page
 	if page.lower().find("internal error") > -1:
 		session.flash = "Error: Invalid Reactome stable identifier"
 	else:	
@@ -21,8 +23,8 @@ def upload():
 		q = page.find('"', p)
 		print ""
 		print ""
-		print page[p:q]
 		if p > -1:
+			print page[p:q]
 			connection.request("GET", page[p:q])
 			session.SBML = connection.getresponse().read()
 
@@ -32,6 +34,8 @@ def upload():
 
 		else:
 			session.flash = "Sorry: SBML for this pathway not found"
+			print ""
+			print "key not found"
 	connection.close()
 	return redirect( URL(r=request, c='Workbench', f='index') )
 
