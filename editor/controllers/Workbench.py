782cf44be36b6a70da6dc1aac8fb609ca4085b9e
matthias bock <matthias.bock@hu-berlin.de>
2011-07-07 19:15:57 +0200

    automatically exporting to JSON

--- editor/controllers/Workbench.py
+++ editor/controllers/Workbench.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
 def index():
+	if session.bioGraph is not None:
+		if session.JSON is None or session.JSON == "":
+			session.JSON = session.bioGraph.exportJSON()
+			# export of SBML or BioPAX is not supported (yet)
 	return dict()
 
