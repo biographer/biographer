b842a2d5396322f29b73ac3f3cecf6e8b7517ef6
matthias bock <matthias.bock@hu-berlin.de>
2011-07-08 10:43:58 +0200

    introduced Reactome SBML file caching

--- editor/controllers/Reactome.py
+++ editor/controllers/Reactome.py
@@ -9,6 +9,7 @@ import biographer
 reload(biographer)
 # END WORKAROUND
 
+import os
 import httplib
 
 def importer():
@@ -18,24 +19,30 @@ def upload():
 	session.JSON = ""
 	session.BioPAX = ""
 	session.RSI = request.vars.RSI
-	connection = httplib.HTTPConnection("www.reactome.org")					# retrieve page for Reactome ID
-	 #"/cgi-bin/eventbrowser_st_id?ST_ID="+str(session.RSI))
-	connection.request("GET", "/cgi-bin/eventbrowser?DB=gk_current&ID=168254&ZOOM=2&CLASSIC=1")
-	page = connection.getresponse().read()
-	if page.lower().find("internal error") > -1:						# page not found
-		session.flash = "Error: Invalid Reactome stable identifier"			# -> invalid ID
-	else:	
-		p = page.find('/cgi-bin/sbml_export?')						# find SBML export link
-		q = page.find('"', p)
-		if p > -1:
-			connection.request("GET", page[p:q])					# download SBML
-			session.SBML		= connection.getresponse().read()
-			if session.bioGraph is None:
-				session.bioGraph = biographer.Graph()
-			session.bioGraph.importSBML( session.SBML )
-			session.flash		= "SBML retrieved successfully"
-		else:										# link not found
-			session.flash		= "Sorry: SBML for this pathway not found"
-	connection.close()
+	cachename = os.path.join( request.folder, "static/Reactome", session.RSI+".sbml" )
+	if os.path.exists( cachename ):
+		session.SBML = open(cachename).read()
+		session.flash = "SBML loaded from cache"
+	else:
+		connection = httplib.HTTPConnection("www.reactome.org")					# retrieve page for Reactome ID
+		 #"/cgi-bin/eventbrowser_st_id?ST_ID="+str(session.RSI))
+		connection.request("GET", "/cgi-bin/eventbrowser?DB=gk_current&ID=168254&ZOOM=2&CLASSIC=1")
+		page = connection.getresponse().read()
+		if page.lower().find("internal error") > -1:						# page not found
+			session.flash = "Error: Invalid Reactome stable identifier"			# -> invalid ID
+		else:	
+			p = page.find('/cgi-bin/sbml_export?')						# find SBML export link
+			q = page.find('"', p)
+			if p > -1:
+				connection.request("GET", page[p:q])					# download SBML
+				session.SBML		= connection.getresponse().read()
+				open(cachename,'w').write(session.SBML)
+				if session.bioGraph is None:
+					session.bioGraph = biographer.Graph()
+				session.bioGraph.importSBML( session.SBML )
+				session.flash		= "SBML retrieved successfully"
+			else:										# link not found
+				session.flash		= "Sorry: SBML for this pathway not found"
+		connection.close()
 	return redirect( URL(r=request, c='Workbench', f='index') )
 
