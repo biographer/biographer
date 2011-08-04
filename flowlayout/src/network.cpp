437c308b061b2339f1766c10959f864c77c33217
ThomasHandorf <ThomasHandorf@localhost>
2011-08-04 15:05:02 +0200

    added some extra whitespaces in network import for lazy empty lines

--- flowlayout/src/network.cpp
+++ flowlayout/src/network.cpp
@@ -127,27 +127,27 @@ void Network::read(const char* file){
    }
 	printf("number of compartments: %d\n",c);
    for(i=0;i<c;i++){
-      scanf("%d %s\n",&_index,t);
+      scanf(" %d %s\n",&_index,t);
       if (_index>c+1){ // not starting at zero
          fprintf(stderr,"compartment index out of bound %d",_index);
          abort();
       }
       addCompartment(_index,t);
    }  
-   scanf("%s\n",s); // "///"  
-   scanf("%d",&n); //number of nodes
+   scanf(" %s\n",s); // "///"  
+   scanf(" %d",&n); //number of nodes
 	printf("number of nodes: %d\n",n);
    if (c>10000){
       fprintf(stderr,"too many nodes %d",c);
       abort();
    }
    for(i=0;i<n;i++){
-      scanf("%d\n",&_index);
+      scanf(" %d\n",&_index);
       if (_index>=n){
          fprintf(stderr,"node index out of bound %d",_index);
          abort();
       }
-      scanf("%s\n",t);
+      scanf(" %s\n",t);
       if(strcmp(t,"Compound")==0)_type=compound;
       else if(strcmp(t,"Reaction")==0)_type=reaction;
       else if(strcmp(t,"Other")==0)_type=other;
@@ -158,8 +158,8 @@ void Network::read(const char* file){
       scanf("%f%f%f",& _width,& _height,& _dir);      
       addNode(_index, _type, s, _width, _height, _x, _y, _dir,ci);            
    }
-   scanf("%s\n",s); // "///"
-   scanf("%d\n",&m); // numer of edges
+   scanf(" %s\n",s); // "///"
+   scanf(" %d\n",&m); // numer of edges
 	printf("number of edges: %d\n",m);
    if (c>40000){
       fprintf(stderr,"too many edges %d",c);
