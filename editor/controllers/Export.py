cfeaf35aca70b1a794ff3949f5d2f8d812e404ef
matthias bock <matthias.bock@hu-berlin.de>
2011-08-08 10:44:18 +0200

    Export functionalities

--- editor/controllers/Export.py
+++ editor/controllers/Export.py
@@ -4,11 +4,24 @@ def index():
 	return redirect(URL(r=request, c="Export", f="JSON"))
 
 def JSON():
-	return session.bioGraph.exportJSON()
+	if session.bioGraph is None:
+		session.flash = "Unable to export: No Model is loaded !"
+		return redirect( URL(r=request, c="Workbench", f="index") )
 
-def SBML():
-	return dict()
+	content = session.bioGraph.exportJSON()
+	IP = response.session_id.split("-")[0]
+	filename = IP+".json"
+	open(request.folder+"/static/Export/"+filename, "w").write(content)
+	return redirect(URL(r=request, c="static", f="Export")+"/"+filename)
 
-def BioPAX():
-	return dict()
+def Layout():
+	if session.bioGraph is None:
+		session.flash = "Unable to export: No Model is loaded !"
+		return redirect( URL(r=request, c="Workbench", f="index") )
+
+	content = session.bioGraph.export_to_Layouter()
+	IP = response.session_id.split("-")[0]
+	filename = IP+".layout"
+	open(request.folder+"/static/Export/"+filename, "w").write(content)
+	return redirect(URL(r=request, c="static", f="Export")+"/"+filename)
 
