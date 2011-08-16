11e057a03dabff23bdef3b7ed99d6e3a280de0c7
matthias bock <matthias.bock@hu-berlin.de>
2011-08-16 11:56:06 +0200

    per default no label

--- editor/modules/constants.py
+++ editor/modules/constants.py
@@ -11,13 +11,13 @@ MandatoryNodeKeys	= ['id','type','sbo','is_abstract']
 NodeKeys		= MandatoryNodeKeys + ['data']
 OptionalNodeKeys	= ['clone_marker', 'x', 'y', 'width', 'height', 'radius', 'label', 'compartment', 'subnodes', 'modifications']
 NodeKeyAliases		= { 'modification':'modifications', 'subcomponents':'subnodes' }
-DefaultNode		= { "type":"simple_species", "sbo":"252", "is_abstract":False, "data":{ "clone_marker":-1, "x":10, "y":10, "width":50, "height":20, "radius":30, "label":"Orphan Node", "compartment":TopCompartmentID, "subnodes":[], "modifications":[] } }
+DefaultNode		= { "type":"simple_species", "sbo":"252", "is_abstract":False, "data":{ "clone_marker":-1, "x":10, "y":10, "width":50, "height":20, "radius":30, "label":"", "compartment":TopCompartmentID, "subnodes":[], "modifications":[] } }
 
 MandatoryEdgeKeys	= ['id','sbo','source','target']
 EdgeKeys		= MandatoryEdgeKeys + ['data']
 OptionalEdgeKeys	= ['type', 'style', 'thickness', 'label', 'label_x', 'label_y', 'handles']
 EdgeKeyAliases		= {}
-DefaultEdge		= { "sbo":13, "type":"Substrate", "source":0, "target":0, "data":{ "type":"straight", "style":"solid", "thickness":1, "label":"Orphan Edge", "label_x":10, "label_y":10, "handles":[] } }
+DefaultEdge		= { "sbo":13, "type":"Substrate", "source":0, "target":0, "data":{ "type":"straight", "style":"solid", "thickness":1, "label":"", "label_x":10, "label_y":10, "handles":[] } }
 
 # end defaults
 
