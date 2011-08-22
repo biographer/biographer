42b5dbb51af3a935e0b7bfd7bbb3175004c6d559
ThomasHandorf <ThomasHandorf@localhost>
2011-08-22 20:08:38 +0200

    included copyright statements and references to the LGPL in each source code file

--- flowlayout/src/edge.h
+++ flowlayout/src/edge.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2011, Wei Jing, Thomas Handorf, The Biographer Community
+ * 
+ * This file is part of Biographer.
+ * Biographer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Biographer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Biographer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef th_edge_h
 #define th_edge_h
 #include "headers.h"
