1984d95a70c9eb68b740a2f08c4298343526a5c7
matthias bock <matthias.bock@hu-berlin.de>
2011-08-24 15:11:35 +0200

    graphviz fixed

--- editor/controllers/Visualization.py
+++ editor/controllers/Visualization.py
@@ -44,14 +44,14 @@ def graphviz():									# graphviz
 	xfactor = 1.34
 	yfactor = 1.33
 	for node in session.bioGraph.Nodes:
-		width	= int(float(node.data['width'])*xfactor)
-		height	= int(float(node.data['height'])*yfactor)
-		left	= str(int(_left + float(node.data['x'])*xfactor - width/2.))
-		top	= str(int(_height - float(node.data['y'])*yfactor - height/2.))
+		width	= int(float(node.data.width)*xfactor)
+		height	= int(float(node.data.height)*yfactor)
+		left	= str(int(_left + float(node.data.x)*xfactor - width/2.))
+		top	= str(int(_height - float(node.data.y)*yfactor - height/2.))
 		width	= str(width)
 		height	= str(height)
 		if left > 0 and top > 0 and width > 0 and height > 0:
-			Map 	+= '<div class=area style="left:'+left+'px; top:'+top+'px; width:'+width+'px; height:'+height+'px;" onClick="gotoDijkstra(\''+str(node.id)+'\');">'+node.data['label']+'</div>\n'
+			Map 	+= '<div class=area style="left:'+left+'px; top:'+top+'px; width:'+width+'px; height:'+height+'px;" onClick="gotoDijkstra(\''+str(node.id)+'\');">'+node.data.label+'</div>\n'
 
 	return dict( DijkstraMap=Map )
 
