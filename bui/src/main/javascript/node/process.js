93eb649684ed3a5899d3edeca4d34696f8866e78
Ben Ripkens <bripkens.dev@gmail.com>
2011-08-26 17:16:14 +0200

    Fixed issue 9.

---
+++ bui/src/main/javascript/node/process.js
@@ -0,0 +1,27 @@
+(function(bui) {
+    var identifier = 'bui.Process';
+    /**
+     * @class
+     * Process node "process"
+     *
+     * @extends bui.RectangularNode
+     * @constructor
+     */
+    bui.Process = function() {
+        bui.Process.superClazz.apply(this, arguments);
+
+        this.labelClass(bui.settings.css.classes.smallText,
+                [bui.settings.css.classes.textDimensionCalculation.small]);
+    };
+
+    bui.Process.prototype = {
+        identifier : function() {
+            return identifier;
+        },
+        _enableResizing : false,
+        _minWidth : bui.settings.style.processNodeMinSize.width,
+        _minHeight : bui.settings.style.processNodeMinSize.height
+    };
+
+    bui.util.setSuperClass(bui.Process, bui.RectangularNode);
+})(bui);
