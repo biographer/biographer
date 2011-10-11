9872fb69e93fcd60ae65cfcc39ffeb6a3a5f7ebd
ThomasHandorf <ThomasHandorf@localhost>
2011-10-11 16:04:45 +0200

    reactionlayout takes initialized Layouter as parameter

--- flowlayout/src/reactionlayout.cpp
+++ flowlayout/src/reactionlayout.cpp
@@ -2,9 +2,7 @@
 #include "plugins.h"
 #include "network.h"
 
-void reactionlayout(Network& nw){
-   Plugins& pgs=register_plugins();
-   Layouter l(nw,pgs);
+void reactionlayout(Layouter& l){
    l.stepAddPlugin(0,P_init_layout);
    l.addEndCondition(0,relForceDiff,0.004);
    
