2ee3ea9983f6b0f22581b6c590302b4f1c97c367
ThomasHandorf <ThomasHandorf@localhost>
2011-10-11 14:23:32 +0200

    removed old code from layout (->plugins) Plugins does not need networks size anymore

--- flowlayout/src/layout.h
+++ flowlayout/src/layout.h
@@ -1,5 +1,8 @@
 #ifndef th_layout_h
 #define th_layout_h
+#include "network.h"
+#include "functions.h"
+#include "plugins.h"
 enum plugins {
    init=1<<0,init_swap=1<<1,init_wComp=1<<2,adjForce=1<<3,nadjForc=1<<4,adjTorque=1<<5,checkCompartment=1<<6,adjustCompartment=1<<7,avoidOverlap=1<<8
 };
@@ -7,8 +10,9 @@ enum conditions{
    iterations=1<<0,relForceDiff=1<<1
 };
 class Layouter;
+
 struct plugin;
-typedef double (*plugin_func_ptr)(Layouter &state,plugin& pg, VP &mov,int round,double temp);
+typedef void (*plugin_func_ptr)(Layouter &state,plugin& pg, VP &mov, VF &rot,int round,double temp);
 struct plugin{
    plugin_func_ptr pfunc;
    VP last;
@@ -16,14 +20,14 @@ struct plugin{
 };
 class Plugins{
    public:
-      Plugins(int _num):num(_num){} // expects number of nodes in network
+      Plugins(){} // expects number of nodes in network
       void registerPlugin(unsigned long pgn, plugin_func_ptr pfunc);
       size_t size();
       plugin& get(int idx);
    private:
-      int num;
       vector<plugin> pluginlist;
 };
+
 struct step{
    VF actplugins;
    unsigned long end;
@@ -33,10 +37,12 @@ struct step{
 
 class Layouter{
    public:
-      Layouter(Network& _nw):nw(_nw), plugins(Plugins(_nw.nodes.size())){
+      Layouter(Network& _nw,Plugins& _pgs):nw(_nw), plugins(_pgs){
          mov.resize(nw.nodes.size());
          movadd.resize(nw.nodes.size());
          avgsize=avg_sizes(nw);
+         get_ideal_distances(nw,dij);
+         get_degrees(nw,deg);
       }
       void setStep(int step,unsigned long bitplugins);
       void addEndCondition(int step, conditions cond, double param);
@@ -45,10 +51,12 @@ class Layouter{
       VP mov;
       VF movadd;
       VF rot;
+      VF dij;
+      VI deg;
       vector<bool> tension;
       double avgsize;
    protected:
-      Plugins plugins;
+      Plugins& plugins;
       void initStep(int step);
       void moveNodes();
       double maxForce;
