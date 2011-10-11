9358fb049044b372a0f8ab93c8736c1652df46d0
ThomasHandorf <ThomasHandorf@localhost>
2011-10-11 17:57:11 +0200

    reactionlayout: added compartment initialization

--- flowlayout/src/reactionlayout.cpp
+++ flowlayout/src/reactionlayout.cpp
@@ -6,5 +6,10 @@ void reactionlayout(Layouter& l){
    l.stepAddPlugin(0,P_init_layout);
    l.stepAddEndCondition(0,relForceDiff,0.0005);
    l.stepLimitMov(0,false);
+
+   l.stepAddPlugin(1,P_adjust_compartments);
+   l.stepAddEndCondition(1,iterations,1); // just one iteration to initialize compartments
+   
    l.execute();
+   
 }
