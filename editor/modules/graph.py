73959757ccbb9cfa6121ef1712c009c94bd65b12
matthias bock <matthias.bock@hu-berlin.de>
2012-01-12 18:46:03 +0100

    graphviz method updated

--- editor/modules/graph.py
+++ editor/modules/graph.py
@@ -418,81 +418,11 @@ class Graph:
 
 		self.initialize()
 
-	def exportGraphviz(self, folder="/tmp", useCache=True, updateNodeProperties=False):
-		self.log("Exporting Graphviz ...")
-		self.status()
 
-		# http://networkx.lanl.gov/pygraphviz/tutorial.html
-		graphviz_model = pygraphviz.AGraph(directed=True)
+	### main model layouting section
+	### invoking the layout subproject, that is seperately developed
 
-		changes = False
-		# !!!!!!!!!!!!!!!!!!!!!!!!!!
-		# Compartments have to be added as
-		# SG = graphviz_model.add_subgraph(name='bla')
-		# !!!!!!!!!!!!!!!!!!!!!!!!!!
-		for node in self.Nodes:
-			# !!!!!!!!!!!!!!!!!!!!!!!!!!!!
-			# This rule actually removes compartments!!!!!!
-			# Come up with something different!
-			# !!!!!!!!!!!!!!!!!!!!!!!!!!!!
-			#if (not node.is_abstract) and (self.EdgeCount(node) > 0):
-			graphviz_model.add_node( 	str(node.id),
-							label = node.data.label if node.data.owns("label") else str(node.id),
-							shape = 'ellipse' if node.type != getNodeType("Process Node") else 'box'
-						)
-			#
-			#elif updateNodeProperties:
-			#	self.Nodes.pop( self.Nodes.index(node) )
-			#	changes = True
-			#	self.log("Warning: Graphviz can't handle Node "+str(node.id)+"! Node deleted.")
-		if changes:
-			self.initialize()	# necessary; e.g. ID map may not fit anymore, because we deleted Nodes
-
-		for edge in self.Edges:
-			graphviz_model.add_edge(	str(edge.source),
-							str(edge.target),
-							arrowhead='normal' if edge.sbo in [ getSBO('Consumption'), getSBO('Production') ] else 'tee'
-						)
-
-		png = self.MD5+".png"
-		dot = self.MD5+".dot"
-		s   = self.MD5+".str"
-		pngpath = os.path.join(folder, png)
-		dotpath = os.path.join(folder, dot)
-		if useCache and os.path.exists( pngpath ):
-			cached = True
-			# no need to do the cpu-intense layouting again
-			self.dot = open(dotpath).read()
-		else:
-			cached = False
-			graphviz_model.dpi = 70;
-			graphviz_model.layout( prog='dot' )
-			graphviz_model.draw( pngpath )
-			self.dot = graphviz_model.string()
-			open(dotpath,'w').write(self.dot)
-
-		# http://www.graphviz.org/doc/info/attrs.html#d:pos
-		changes = False
-		if updateNodeProperties:
-			for node in self.Nodes:
-				p = self.dot.find("\t"+str(node.id)+"\t")
-				if p > -1:
-					q = self.dot.find(";", p)
-					node.setByGraphviz( self.dot[p:q] )
-				else:
-					self.Nodes.pop( self.Nodes.index(node) )
-					changes = True
-					self.log("Warning: Updating Node "+str(node.id)+" from graphviz output failed! Node deleted.")
-		if changes:
-			self.initialize()
-
-		return self.dot, png, cached, None
-
-
-	### main model layouting section ###
-	### invoking the layout subproject, that is seperately developed ###
-
-	### here: http://code.google.com/p/biographer/source/browse?repo=layout ###
+	### here: http://code.google.com/p/biographer/source/browse?repo=layout
 
 
 	# Exchange Format:
@@ -581,13 +511,13 @@ class Graph:
 
 		self.initialize()
 
-	def execute_Layouter(self, path_to_layout_binary, execution_path='/tmp'):
+	def execute_layout(self, path_to_layout_binary, execution_folder='/tmp'):
 
-		infile = os.path.join(execution_path, 'layout.infile')
-		outfile = os.path.join(execution_path, 'layout.outfile')
+		infile = os.path.join(execution_folder, 'layout.infile')
+		outfile = os.path.join(execution_folder, 'layout.outfile')
 
-		self.log("Now executing the Layouter: "+path_to_layout_binary)
-		self.log("in "+execution_path+" ...")
+		self.log("Now executing the layouter: "+path_to_layout_binary)
+		self.log("in "+execution_folder+" ...")
 
 		open(infile, 'w').write( self.export_to_Layouter() )
 		os.path.delete(outfile)
@@ -595,16 +525,17 @@ class Graph:
 		timeout = 30
 		start = time()									# start a timer
 		process = Popen( split(path_to_layout_binary) )					# run layout binary
+		self.log("Executable started. Waiting for process to complete ...")
 		runtime = 0
 		while (layouter.poll is None) and (runtime < timeout):				# wait until timeout
-			print "Layouter runtime is now: "+str(runtime)+" ... Waiting for process to complete ..."
 			sleep(2)
 			runtime = time()-start
+			self.log("Timeout is set to "+str(timeout)+"s. Runtime is now: "+str(runtime)+"s.")
 
 		if runtime < timeout:
 			self.log(path_to_layout_binary+" finished.")
 		else:
-			self.log("Sorry, process timed out. Timeout was "+str(timeout)+"s.")
+			self.log("Sorry, process timed out.")
 			return False
 
 		self.import_from_Layouter( open(outfile).read() )
@@ -612,6 +543,84 @@ class Graph:
 		os.path.delete(infile)
 
 		self.log("Layouting completed successfully.")
+
+
+	### secondary model layouting
+	### using graphviz
+
+	def execute_graphviz(self, execution_folder="/tmp", use_cache=True, update_nodes=True):
+		self.log("Preparing graphviz model ...")
+
+		# http://networkx.lanl.gov/pygraphviz/tutorial.html
+		graphviz_model = pygraphviz.AGraph(directed=True)
+
+		changes = False
+		# !!!!!!!!!!!!!!!!!!!!!!!!!!
+		# Compartments have to be added as
+		# SG = graphviz_model.add_subgraph(name='bla')
+		# !!!!!!!!!!!!!!!!!!!!!!!!!!
+		for node in self.Nodes:
+			# !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			# This rule actually removes compartments!!!!!!
+			# Come up with something different!
+			# !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			#if (not node.is_abstract) and (self.EdgeCount(node) > 0):
+			graphviz_model.add_node( 	str(node.id),
+							label = node.data.label if node.data.owns("label") else str(node.id),
+							shape = 'ellipse' if node.type != getNodeType("Process Node") else 'box'
+						)
+			#
+			#elif updateNodeProperties:
+			#	self.Nodes.pop( self.Nodes.index(node) )
+			#	changes = True
+			#	self.log("Warning: Graphviz can't handle Node "+str(node.id)+"! Node deleted.")
+		if changes:
+			self.initialize()	# necessary; e.g. ID map may not fit anymore, because we deleted Nodes
+
+		for edge in self.Edges:
+			graphviz_model.add_edge(	str(edge.source),
+							str(edge.target),
+							arrowhead='normal' if edge.sbo in [getSBO('Consumption'), getSBO('Production')] else 'tee'
+						)
+
+		png = self.MD5+".png"
+		dot = self.MD5+".dot"
+		s   = self.MD5+".str"
+		pngpath = os.path.join(execution_folder, png)
+		dotpath = os.path.join(execution_folder, dot)
+
+		if use_cache and os.path.exists( pngpath ):	# no need to do the cpu-intense layouting again
+			cached = True
+			self.dot = open(dotpath).read()
+			self.log("A matching graphviz layout was found in cache. use_cache is enabled. Not executing graphviz.")
+		else:
+			self.log("Executing graphviz ...")
+			cached = False
+			graphviz_model.dpi = 70;
+			graphviz_model.layout( prog='dot' )
+			graphviz_model.draw( pngpath )
+			self.dot = graphviz_model.string()
+			open(dotpath,'w').write(self.dot)
+			self.log("graphviz completed.")
+
+		# http://www.graphviz.org/doc/info/attrs.html#d:pos
+		changes = False
+		if update_nodes:
+			for node in self.Nodes:
+				p = self.dot.find("\t"+str(node.id)+"\t")
+				if p > -1:
+					q = self.dot.find(";", p)
+					node.update_using_graphviz_output( self.dot[p:q] )
+				else:
+					self.Nodes.pop( self.Nodes.index(node) )
+					changes = True
+					self.log("Error: Updating Node "+str(node.id)+" failed! Node deleted.")
+			self.log("Model updated.")
+
+		if changes:
+			self.initialize()
+
+		return self.dot, png, cached, None
 
 
 	### basic functions on Graph properties ###
