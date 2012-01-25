74d72188e77e35fee8781d093e368ca9d6c85ea4
fkrause <fkrause@localhost>
2012-01-25 10:28:28 +0100

    removed default controller since the editor controller will become the default controller

--- editor/controllers/editor.py
+++ editor/controllers/editor.py
@@ -184,5 +184,19 @@ def export():
     return out
     response.write(out,escape=False)
 
-def text_width():
-    return dict()
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login 
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
