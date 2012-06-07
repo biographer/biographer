1429fc974239deffe1bbf66b9e4928c3f85ea3f2
ThomasHandorf <ThomasHandorf@localhost>
2012-06-07 17:59:31 +0200

    reactionlayout: copied current step config from testlayout

--- flowlayout/src/reactionlayout.cpp
+++ flowlayout/src/reactionlayout.cpp
@@ -15,9 +15,10 @@ void reactionlayout(Layouter& l){
    l.addEndCondition(C_iterations,1550);
    
    l.addStep();
-   l.addPlugins(P_force_adj, P_expand, P_limit_mov);
+   l.addPlugins(P_force_adj, P_distribute_edges, P_expand, P_limit_mov);
    l.pluginScale(P_force_adj, 10);
    l.pluginScale(P_expand, 1.0/l.nw.nodes.size());
+   l.pluginScale(P_distribute_edges, 10);
    l.addEndCondition(C_avgMovLimit,0.05);
    l.addEndCondition(C_relForceDiff,0.005);
    l.addEndCondition(C_iterations,500);
@@ -48,10 +49,11 @@ void reactionlayout(Layouter& l){
    l.addEndCondition(C_iterations,1);
    
    l.addStep();
-   l.addPlugins(P_force_adj, P_adjust_compartments_fixed, P_force_compartments, P_expand, P_limit_mov);
+   l.addPlugins(P_force_adj, P_distribute_edges, P_force_compartments, P_adjust_compartments_fixed, P_expand, P_limit_mov);
    l.pluginScale(P_force_adj, 10);
    l.fixPluginTemp(P_force_compartments,0);
    l.pluginScale(P_expand, 1.0/l.nw.nodes.size());
+   l.pluginScale(P_distribute_edges, 10);
    l.addEndCondition(C_avgMovLimit,0.05);
    l.addEndCondition(C_temp,3);
    l.addEndCondition(C_iterations,100);
@@ -61,22 +63,22 @@ void reactionlayout(Layouter& l){
    l.fixPluginTemp(P_force_compartments,0);
    l.addEndCondition(C_iterations,1);
    
-   l.addStep();
-   l.addPlugins(P_force_adj, P_force_nadj, P_torque_adj, P_distribute_edges, P_min_edge_crossing_multi, P_adjust_compartments_fixed, 
-                    P_force_compartments, P_limit_mov);
-   l.addEndCondition(C_relForceDiff,0.005);
-   l.addEndCondition(C_temp,10);
-   //l.pluginScale(P_min_edge_crossing_multi, 0.1);
-   //l.pluginScale(P_force_compartments, 10);
-   l.addEndCondition(C_iterations,550);
+//    l.addStep();
+//    l.addPlugins(P_force_adj_strong, P_force_nadj, P_torque_adj, P_distribute_edges, P_min_edge_crossing_multi, P_force_compartments, 
+//                 P_adjust_compartments_fixed, P_limit_mov);
+//    l.addEndCondition(C_relForceDiff,0.005);
+//    l.addEndCondition(C_temp,10);
+//    //l.pluginScale(P_min_edge_crossing_multi, 0.1);
+//    //l.pluginScale(P_force_compartments, 10);
+//    l.addEndCondition(C_iterations,550);
 
    l.addStep();
    l.addPlugins(P_unfix_all);
    l.addEndCondition(C_iterations,1);
    
    l.addStep();
-   l.addPlugins(P_force_adj, P_force_nadj, P_torque_adj, P_distribute_edges, P_min_edge_crossing_multi, P_adjust_compartments_fixed, 
-                    P_force_compartments, P_limit_mov, P_node_collision);
+   l.addPlugins(P_force_adj_strong, P_force_nadj, P_torque_adj, P_distribute_edges, P_min_edge_crossing_multi, P_force_compartments, 
+                P_adjust_compartments_fixed, P_limit_mov, P_node_collision);
    //l.pluginScale(P_min_edge_crossing_multi, 0.1);
    //l.pluginScale(P_force_compartments, 10);
    l.addEndCondition(C_maxMovLimit,0.005);
@@ -84,11 +86,11 @@ void reactionlayout(Layouter& l){
 
    
    l.addStep();
-   l.addPlugins(P_force_adj, P_force_nadj, P_torque_adj, P_distribute_edges, P_min_edge_crossing_multi, 
+   l.addPlugins(P_force_adj_strong, P_force_nadj, P_torque_adj, P_distribute_edges, P_min_edge_crossing_multi, 
                 P_force_compartments, P_compartment_collision, P_node_collision);
    //l.pluginScale(P_min_edge_crossing_multi, 0.1);
    l.addEndCondition(C_maxMovLimit,0.005);
-   l.pluginScale(P_force_adj, 0.5);
+   l.pluginScale(P_force_adj_strong, 0.5);
    l.fixPluginTemp(P_force_compartments,0);
    //l.pluginScale(P_force_compartments, 10);
    l.addEndCondition(C_iterations,550);
