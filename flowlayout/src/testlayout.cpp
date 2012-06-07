3feee34cdc004da3a9c8b716b7ce9f9aed95933b
ThomasHandorf <ThomasHandorf@localhost>
2012-06-07 11:29:42 +0200

    new setup for layout program; added force_adj_strong

--- flowlayout/src/testlayout.cpp
+++ flowlayout/src/testlayout.cpp
@@ -90,21 +90,21 @@ int main(int argc,char *argv[]){
    l.fixPluginTemp(P_force_compartments,0);
    l.addEndCondition(C_iterations,1);
    
-   l.addStep();
-   l.addPlugins(P_force_adj, P_force_nadj, P_torque_adj, P_distribute_edges, P_min_edge_crossing_multi, P_force_compartments, 
-                P_adjust_compartments_fixed, P_limit_mov);
-   l.addEndCondition(C_relForceDiff,0.005);
-   l.addEndCondition(C_temp,10);
-   //l.pluginScale(P_min_edge_crossing_multi, 0.1);
-   //l.pluginScale(P_force_compartments, 10);
-   l.addEndCondition(C_iterations,550);
+//    l.addStep();
+//    l.addPlugins(P_force_adj_strong, P_force_nadj, P_torque_adj, P_distribute_edges, P_min_edge_crossing_multi, P_force_compartments, 
+//                 P_adjust_compartments_fixed, P_limit_mov);
+//    l.addEndCondition(C_relForceDiff,0.005);
+//    l.addEndCondition(C_temp,10);
+//    //l.pluginScale(P_min_edge_crossing_multi, 0.1);
+//    //l.pluginScale(P_force_compartments, 10);
+//    l.addEndCondition(C_iterations,550);
 
    l.addStep();
    l.addPlugins(P_unfix_all);
    l.addEndCondition(C_iterations,1);
    
    l.addStep();
-   l.addPlugins(P_force_adj, P_force_nadj, P_torque_adj, P_distribute_edges, P_min_edge_crossing_multi, P_force_compartments, 
+   l.addPlugins(P_force_adj_strong, P_force_nadj, P_torque_adj, P_distribute_edges, P_min_edge_crossing_multi, P_force_compartments, 
                 P_adjust_compartments_fixed, P_limit_mov, P_node_collision);
    //l.pluginScale(P_min_edge_crossing_multi, 0.1);
    //l.pluginScale(P_force_compartments, 10);
@@ -113,11 +113,11 @@ int main(int argc,char *argv[]){
 
    
    l.addStep();
-   l.addPlugins(P_force_adj, P_force_nadj, P_torque_adj, P_distribute_edges, P_min_edge_crossing_multi, 
+   l.addPlugins(P_force_adj_strong, P_force_nadj, P_torque_adj, P_distribute_edges, P_min_edge_crossing_multi, 
                 P_force_compartments, P_compartment_collision, P_node_collision);
    //l.pluginScale(P_min_edge_crossing_multi, 0.1);
    l.addEndCondition(C_maxMovLimit,0.005);
-   l.pluginScale(P_force_adj, 0.5);
+   l.pluginScale(P_force_adj_strong, 0.5);
    l.fixPluginTemp(P_force_compartments,0);
    //l.pluginScale(P_force_compartments, 10);
    l.addEndCondition(C_iterations,550);
