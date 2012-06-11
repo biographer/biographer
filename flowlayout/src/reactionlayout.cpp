9f5bc16672b722ddfefa0c8faa7f703aadc8b414
ThomasHandorf <ThomasHandorf@localhost>
2012-06-11 17:18:01 +0200

    reactionlayout: copied current step config from testlayout

--- flowlayout/src/reactionlayout.cpp
+++ flowlayout/src/reactionlayout.cpp
@@ -15,7 +15,7 @@ void reactionlayout(Layouter& l){
    l.addEndCondition(C_iterations,1550);
    
    l.addStep();
-   l.addPlugins(P_force_adj, P_distribute_edges, P_expand, P_limit_mov);
+   l.addPlugins(P_force_adj, P_distribute_edges, P_expand, P_push_components, P_limit_mov);
    l.pluginScale(P_force_adj, 10);
    l.pluginScale(P_expand, 1.0/l.nw.nodes.size());
    l.pluginScale(P_distribute_edges, 10);
@@ -49,7 +49,7 @@ void reactionlayout(Layouter& l){
    l.addEndCondition(C_iterations,1);
    
    l.addStep();
-   l.addPlugins(P_force_adj, P_distribute_edges, P_force_compartments, P_adjust_compartments_fixed, P_expand, P_limit_mov);
+   l.addPlugins(P_force_adj, P_distribute_edges, P_force_compartments, P_adjust_compartments_fixed, P_expand, P_push_components, P_limit_mov);
    l.pluginScale(P_force_adj, 10);
    l.fixPluginTemp(P_force_compartments,0);
    l.pluginScale(P_expand, 1.0/l.nw.nodes.size());
@@ -87,7 +87,7 @@ void reactionlayout(Layouter& l){
    
    l.addStep();
    l.addPlugins(P_force_adj_strong, P_force_nadj, P_torque_adj, P_distribute_edges, P_min_edge_crossing_multi, 
-                P_force_compartments, P_compartment_collision, P_node_collision);
+                P_force_compartments, P_limit_mov, P_compartment_collision, P_node_collision);
    //l.pluginScale(P_min_edge_crossing_multi, 0.1);
    l.addEndCondition(C_maxMovLimit,0.005);
    l.pluginScale(P_force_adj_strong, 0.5);
