fd5265fc7051b921439678c3803cf3d26de1eba7
Taye Adeyemi <adeyemi.taye@gmail.com>
2012-06-23 17:21:22 +0000

    set rectangularNodes as interactable

--- bui/src/main/javascript/node/rectangularNode.js
+++ bui/src/main/javascript/node/rectangularNode.js
@@ -104,6 +104,25 @@
         var size = this.size();
         formChanged.call(this, this, size.width, size.height);
         container.appendChild(privates.rect);
+
+        // set as interactable
+        interact.set(privates.rect, {drag: this._enableDragging, resize: this._enableResizing});
+
+        // create eventListener delegate functions
+        interactDragMove = (function (event) {
+            var position = this.position();
+            this.position(position.x + event.detail.dx, position.y + event.detail.dy);
+        }).createDelegate(this);
+        
+        interactResizeMove = (function (event) {
+            var size = this.size();
+            this.size(size.width + event.detail.dx, size.height + event.detail.dy);
+        }).createDelegate(this);
+        
+        // add event listeners
+        privates.rect.addEventListener('interactresizemove', interactResizeMove);
+        privates.rect.addEventListener('interactdragmove', interactDragMove);
+        
     };
 
     /**
