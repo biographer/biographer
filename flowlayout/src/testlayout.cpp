3649a10414a9ae9efc61948eaa5af4e49afa2f22
ThomasHandorf <ThomasHandorf@localhost>
2012-06-25 15:42:39 +0200

    testlayout: new compbination of expand and subsequently adding torque and distribute_edges

--- flowlayout/src/testlayout.cpp
+++ flowlayout/src/testlayout.cpp
@@ -42,17 +42,29 @@ int main(int argc,char *argv[]){
    l.addEndCondition(C_iterations,1550);
    
    l.addStep();
-   l.addPlugins(P_force_adj, P_distribute_edges, P_expand, P_push_components, P_limit_mov);
+   l.addPlugins(P_force_adj, P_expand, P_push_components, P_limit_mov);
    l.pluginScale(P_force_adj, 10);
    l.pluginScale(P_expand, 1.0/l.nw.nodes.size());
+   l.addEndCondition(C_iterations,500);
+
+   l.addStep();
+   l.addPlugins(P_force_adj, P_torque_adj, P_expand, P_push_components, P_limit_mov);
+   l.pluginScale(P_force_adj, 10);
+   l.pluginScale(P_expand, 1.0/l.nw.nodes.size());
+   l.pluginScale(P_torque_adj, 10);
+   l.addEndCondition(C_iterations,500);
+   
+   l.addStep();
+   l.addPlugins(P_force_adj, P_torque_adj, P_distribute_edges, P_expand, P_push_components, P_limit_mov);
+   l.pluginScale(P_force_adj, 10);
+   l.pluginScale(P_expand, 1.0/l.nw.nodes.size());
+   l.pluginScale(P_torque_adj, 10);
    l.pluginScale(P_distribute_edges, 10);
-   l.addEndCondition(C_avgMovLimit,0.05);
-   l.addEndCondition(C_relForceDiff,0.005);
    l.addEndCondition(C_iterations,500);
    
    l.addStep();
 //   l.addPlugins(P_force_adj, P_torque_adj, P_distribute_edges, P_min_edge_crossing_multi, P_limit_mov);
-   l.addPlugins(P_force_adj, P_torque_adj, P_limit_mov);
+   l.addPlugins(P_force_adj, P_torque_adj, P_distribute_edges, P_limit_mov);
    l.addEndCondition(C_avgMovLimit,0.005);
 //   l.pluginScale(P_min_edge_crossing_multi, 0.1);
    l.addEndCondition(C_iterations,1550);
@@ -108,16 +120,17 @@ int main(int argc,char *argv[]){
                 P_adjust_compartments_fixed, P_limit_mov, P_node_collision);
    //l.pluginScale(P_min_edge_crossing_multi, 0.1);
    //l.pluginScale(P_force_compartments, 10);
+   l.pluginScale(P_min_edge_crossing_multi, 10);
    l.addEndCondition(C_maxMovLimit,0.005);
-   l.addEndCondition(C_iterations,550);
+   l.addEndCondition(C_iterations,2550);
 
    
    l.addStep();
    l.addPlugins(P_force_adj_strong, P_force_nadj, P_torque_adj, P_distribute_edges, P_min_edge_crossing_multi, 
                 P_force_compartments, P_limit_mov, P_compartment_collision, P_node_collision);
-   //l.pluginScale(P_min_edge_crossing_multi, 0.1);
+   l.pluginScale(P_min_edge_crossing_multi, 10);
    l.addEndCondition(C_maxMovLimit,0.005);
-   l.pluginScale(P_force_adj_strong, 0.5);
+//   l.pluginScale(P_force_adj_strong, 0.5);
    l.fixPluginTemp(P_force_compartments,0);
    //l.pluginScale(P_force_compartments, 10);
    l.addEndCondition(C_iterations,550);
