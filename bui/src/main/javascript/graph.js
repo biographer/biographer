b47b0424d8faf4d0c66924fb5d35598825c657b9
ThomasHandorf <ThomasHandorf@localhost>
2012-08-01 11:24:03 +0200

    fix: svg-export: in firefox css was not correctly loaded and included

--- bui/src/main/javascript/graph.js
+++ bui/src/main/javascript/graph.js
@@ -512,6 +512,7 @@
             jQuery.ajax({
                         url : bui.settings.css.stylesheetUrl,
                         async : false,
+                        dataType : 'text',
                         success : function(data) {
                             css = data;
                         }
