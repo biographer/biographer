b9a09c9c26a0bc8b3aa6d47f70276e492fd967b2
fkrause <fkrause@localhost>
2012-08-22 13:42:15 +0200

    must return new nodes so they can be hooked up onclick functions

--- bui/src/main/javascript/util.js
+++ bui/src/main/javascript/util.js
@@ -515,6 +515,7 @@
     bui.util.clone = function(graph, degree, select_drawables){
             var suspendHandle = graph.suspendRedraw(20000);
             all_drawables = graph.drawables();
+            var new_nodes = [];
             // create a counting dictionary for the nodes
             var degree_count = {};
             var drawable, edge, old_node_id, new_node;
@@ -530,45 +531,45 @@
                 if (drawable.identifier() == 'bui.Edge'){
                     if (drawable.source().id() in degree_count){
                         degree_count[drawable.source().id()] = degree_count[drawable.source().id()] + 1;
-                    };
+                    }
                     if (drawable.target().id() in degree_count){
                         degree_count[drawable.target().id()] = degree_count[drawable.target().id()] + 1;
-                    };
+                    }
                 }
             }
             // go through all the nodes with a higher than appreciated degree
             var auto_indent = 1000;
             for (var key in degree_count) {
-                if(select_drawables != undefined && !(key in select_drawables)) continue;
+                if(select_drawables !== undefined && !(key in select_drawables)) continue;
                 drawable = all_drawables[key];
                 if (degree_count[drawable.id()] > degree){
                     old_node_id = drawable.id();
-
                     // create a new node for every time the old node is referenced
                     for (var edge_key in all_drawables){
                         edge = all_drawables[edge_key];
                         if ((edge.identifier() == 'bui.Edge')&&((edge.source().id() == old_node_id)||(edge.target().id() == old_node_id))){
-                // create a new node
-                ++auto_indent;
-                new_node = graph.add(bui[drawable.identifier().substr(4)]) 
-                    .visible(true)
-                    .label(drawable.label())
-                    .parent(drawable.parent())
-                    //.addClass('cloneMarker')
-                    .position(drawable.position().x, drawable.position().y)
-                    .size(drawable.size().height, drawable.size().width);
-                // reroute the edge
-                if (edge.source().id() == old_node_id){
-                    all_drawables[edge_key].source(new_node);
-                } else {
-                    all_drawables[edge_key].target(new_node);
+                            // create a new node
+                            ++auto_indent;
+                            new_node = graph.add(bui[drawable.identifier().substr(4)])
+                                .visible(true)
+                                .label(drawable.label())
+                                .parent(drawable.parent())
+                                //.addClass('cloneMarker')
+                                .position(drawable.position().x, drawable.position().y)
+                                .size(drawable.size().height, drawable.size().width);
+                            // reroute the edge
+                            new_nodes.push(new_node);
+                            if (edge.source().id() == old_node_id){
+                                all_drawables[edge_key].source(new_node);
+                            } else {
+                                all_drawables[edge_key].target(new_node);
+                            }
+                        }
+                    }
                 }
-                }
-            };
-                };
-            };
+            }
             for (var key in all_drawables) {
-                if(select_drawables != undefined && !(key in select_drawables)) continue;
+                if(select_drawables !== undefined && !(key in select_drawables)) continue;
                 drawable = all_drawables[key];
                 if (drawable.id() in degree_count){
                     if (degree_count[drawable.id()] > degree){
@@ -577,7 +578,8 @@
                 }
             }
             graph.unsuspendRedraw(suspendHandle);
-    } 
+            return new_nodes;
+    };
 
 
     bui.util.combine = function(graph, select_drawables){
