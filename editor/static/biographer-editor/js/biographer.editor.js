8c8e1d11493d9d0024b6234220101c440e529c92
Basti <Basti@localhost>
2013-12-03 15:07:34 +0100

    minor changes in one click deslection

--- editor/static/biographer-editor/js/biographer.editor.js
+++ editor/static/biographer-editor/js/biographer.editor.js
@@ -55,7 +55,7 @@ Editor.prototype = {
     },
     doHeartBeat: function() {
         if (!this.timer_is_on) {
-          this.timer_is_on=1;
+          this.timer_is_on=2;
           this.autosaveHeartBeat(true);
           }
     },
@@ -458,12 +458,15 @@ Editor.prototype = {
                         this_editor.select(drawable);
                     }
                 }else{
-                    //shift key is not down
+                    //shift key is not down everything is unselected
+
                     this_editor.selectAll(false);
+                   
                     if (event !== undefined){
                         event.stopPropagation();
+                    
                     }
-                    this_editor.select(drawable);
+//                    this_editor.select(drawable); //commented out because does not work
                 }
             }
             if (this_editor.cur_mode == 'Edge'){
@@ -1821,6 +1824,7 @@ Editor.prototype = {
             var all_drawables = this_editor.graph.drawables();
             if(this_editor.selected_nodes.length != Object.keys(all_drawables).length) visible = true;
             else visible = false;
+            console.log("selected edge")
             this_editor.selectAll(visible);
         });
         //=========================
@@ -2058,7 +2062,6 @@ Editor.prototype = {
         $('#canvas').click(function(){
             console.log("HIER")
                 //if this_editor.shifted == false{
-                //     console.log("HIER")
                 //     this_editor.selectAll(false);
                 // }
                 })
@@ -2077,7 +2080,7 @@ Editor.prototype = {
                 }
                 })
         //------------
-        }
+        //}
 		//-------------------------------------------------
         // websocket chat
         // FIXME must include this funciton here
